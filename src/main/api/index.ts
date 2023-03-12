ba99d1abe191852931154836a13efec86f64cf83
Sergio Kopplin <sergiokopplin@gmail.com>
2023-03-12 15:53:20 -0300

    removes unused files

--- src/main/api/index.ts
+++
@@ -1,7 +0,0 @@
-import '../config/module-alias'
-
-import { PersonController } from '@/application/controllers/person'
-
-const p = new PersonController()
-console.log(p.speak('rodrigo'))
-console.log(p.speak())
