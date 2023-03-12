b0c5a740a2bc265781dcdfa4a31eb3b2e8009ab6
Sergio Kopplin <sergiokopplin@gmail.com>
2023-03-12 16:39:40 -0300

    adds FacebookAuthentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,14 @@
+import { type AuthenticationError } from '@/domain/errors'
+import { type AccessToken } from '@/domain/models'
+
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<any>
+}
+
+namespace FacebookAuthentication {
+  export interface Params {
+    token: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+}
